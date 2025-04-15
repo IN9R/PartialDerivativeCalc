3c23eef304554fcd1922b1278003ca4a4e1e7f1c
IN9R <58347338+IN9R@users.noreply.github.com>
2025-04-15 21:55:14 +0200

    Make Render work

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from sympy import symbols, diff, latex
 from latex2sympy2 import latex2sympy
+import os
 
 app = Flask(__name__)
 
@@ -59,4 +60,5 @@ def index():
     return render_template("index.html", result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
